daf2f9c446cfa3a7d68c488bfe938cdee11fd27a
Vigen <vigenpooya@gmail.com>
2021-09-11 11:33:53 +0430

    add tools

---
+++ src/components/Tools/index.js
@@ -0,0 +1,8 @@
+import Button from './Button'
+import { Field, Upload } from './Form'
+import Icon from './Icon'
+import Link from './Link'
+import Modal from './Modal'
+import Avatar from './Avatar'
+
+export { Button, Field, Upload, Icon, Link, Modal, Avatar }
