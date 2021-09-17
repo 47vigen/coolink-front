eac0753dce5df121fe13995317b4ea8092583b4a
Vigen <vigenpooya@gmail.com>
2021-09-17 12:32:16 +0430

    add Page page

---
+++ src/pages/[slug]/index.js
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import Page from '../../components/Layout/Page'
+import Render from '../../components/Template/Render'
+
+// ** Graphql
+import { client } from '../../graphql/apollo'
+import { SHOW_PAGE, SHOW_SECTIONS } from '../../graphql/queries'
+
+export default function Home({ page, sections }) {
+  console.log({ page, sections })
+
+  return (
+    <Page page={page}>
+      <Render page={page} sections={sections} />
+    </Page>
+  )
+}
+
+export async function getServerSideProps({ params }) {
+  const { data: dataPage, error: errorPage } = await client.query({
+    query: SHOW_PAGE,
+    variables: {
+      slug: params.slug
+    }
+  })
+
+  if (dataPage?.showPage && !errorPage) {
+    const { data: dataSections, error: errorSections } = await client.query({
+      query: SHOW_SECTIONS,
+      variables: {
+        page: dataPage.showPage.id
+      }
+    })
+
+    if (dataSections?.showSection && !errorSections) {
+      return {
+        props: {
+          page: dataPage.showPage,
+          sections: dataSections.showSection
+        }
+      }
+    }
+  }
+
+  return {
+    notFound: true
+  }
+}
