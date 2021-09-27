e615fa62b1d327da9a2167a6141b7e1592e82f40
Vigen <vigenpooya@gmail.com>
2021-09-27 20:38:03 +0330

    add cover and avatar position

--- src/components/Layout/Header/PageHeader.js
+++ src/components/Layout/Header/PageHeader.js
@@ -1,30 +1,67 @@
 import React from 'react'
+import classNames from '../../../utils/classNames'
+import { getImgSrc } from '../../../utils/getImgSrc'
 
 // ** UI
-import { Link, Icon, Avatar } from '../../Tools'
+import { Link, Icon, Avatar, Element } from '../../Tools'
 
 function PageHeader({ page, linked, onEdit }) {
+  const titles = React.useMemo(
+    () => ({ title: page.avatar?.customize?.color || 'content', subTitle: page.avatar?.customize?.second || 'line' }),
+    [page]
+  )
+
   return (
-    <header className="flex items-center container lg:max-w-md mx-auto">
-      <div className="relative w-20 me-4">
-        {linked ? (
-          <Link href={`/${page.slug}`}>
-            <Avatar url={page.profilePic} className="w-20 h-20" />
-          </Link>
-        ) : (
-          <Avatar url={page.profilePic} className="w-20 h-20" />
+    <header>
+      {page.style?.cover?.url ? (
+        <Element
+          hoverable={false}
+          className="h-40 -mb-10 rounded-t-none -mt-10 -mx-4 bg-cover"
+          customize={{ ...page.style?.customize, ...page.style?.cover?.customize }}
+          style={{
+            backgroundImage: `url('${getImgSrc(page.style.cover.url)}')`
+          }}
+        />
+      ) : null}
+      <div
+        className={classNames(
+          'flex items-center container lg:max-w-md mx-auto',
+          page.avatar?.position === 'center' ? 'flex-col items-center' : 'space-s-4'
         )}
-        {onEdit ? (
-          <button className="absolute bottom-0 left-0 transition duration-300 hover:opacity-60" onClick={onEdit}>
-            <Icon name="edit" className="bg-body p-1.5 rounded-md" />
-          </button>
-        ) : null}
-      </div>
-      <div className="flex flex-col flex-1">
-        <h1 className="text-lg mb-1">{page?.title}</h1>
-        <span className="text-line text-base">{page?.subTitle}</span>
+      >
+        <AvatarSection page={page} linked={linked} onEdit={onEdit} />
+        {page.avatar?.position === 'center' ? (
+          <>
+            <h1 className={classNames('text-lg mt-1', `text-${titles.title}`)}>{page?.title}</h1>
+            <span className={classNames('text-base', `text-${titles.subTitle}`)}>{page?.subTitle}</span>
+          </>
+        ) : (
+          <div className="flex flex-col flex-1">
+            <h1 className={classNames('text-lg mb-1', `text-${titles.title}`)}>{page?.title}</h1>
+            <span className={classNames('text-base', `text-${titles.subTitle}`)}>{page?.subTitle}</span>
+          </div>
+        )}
       </div>
     </header>
+  )
+}
+
+const AvatarSection = ({ page, linked, onEdit }) => {
+  return (
+    <div className="relative w-min">
+      {linked ? (
+        <Link href={`/${page.slug}`}>
+          <Avatar url={page.avatar?.url} className="w-20 h-20" rounded={page.avatar?.customize?.rounded} />
+        </Link>
+      ) : (
+        <Avatar url={page.avatar?.url} className="w-20 h-20" rounded={page.avatar?.customize?.rounded} />
+      )}
+      {onEdit ? (
+        <button className="absolute bottom-0 left-0 transition duration-300 hover:opacity-60" onClick={onEdit}>
+          <Icon name="edit" className="bg-body p-1.5 rounded-ts-md rounded-be-md" />
+        </button>
+      ) : null}
+    </div>
   )
 }
 
