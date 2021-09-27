a7324b6337cc669bb6d819535cbccd16bfd88a4f
Vigen <vigenpooya@gmail.com>
2021-09-27 20:36:05 +0330

    fix to section customize

--- src/components/Feeds/index.js
+++ src/components/Feeds/index.js
@@ -11,7 +11,7 @@ import classNames from '../../utils/classNames'
 import { useQuery } from '@apollo/client'
 import { GET_PAGE_FEEDS } from '../../graphql/queries'
 
-function Feeds({ page, children }) {
+function Feeds({ page, section, children }) {
   const [feeds, setFeeds] = React.useState([])
   const {
     data,
@@ -36,18 +36,26 @@ function Feeds({ page, children }) {
 
   const [isOpened, setIsOpened] = React.useState('')
 
+  const custom = React.useCallback((idx) => (section.customized ? section.customize[idx] || {} : {}), [section.customized, section.customize])
+
   return (
     <>
       <div className={classNames('grid grid-cols-3 gap-2', feeds.length ? 'my-4' : 'mt-4')}>
         {feeds.map((feed) => (
-          <Feed key={feed.id} feed={feed} color={page.customize?.color} opened={isOpened === feed.id} onOpen={() => setIsOpened(feed.id)}>
-            {typeof children === 'function' ? children(feed) : children}
+          <Feed
+            feed={feed}
+            key={feed.id}
+            opened={isOpened === feed.id}
+            onOpen={() => setIsOpened(feed.id)}
+            customize={{ second: page.style?.customize?.color, color: 'white', ...custom(1) }}
+          >
+            {typeof children === 'function' ? children(feed, custom) : children}
           </Feed>
         ))}
       </div>
       <Element
         tag="button"
-        customize={page.customize}
+        customize={{ ...page.style.customize, ...custom(0) }}
         onClick={getPageFeedsLoading || fetchMoreLoading ? null : fetchMore}
         className="flex w-full justify-center items-center min-h-[2rem] mb-4"
       >
