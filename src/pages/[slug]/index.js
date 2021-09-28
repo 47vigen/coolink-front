cb7672f5568b2e8e557eb689854637268ec9b454
Vigen <vigenpooya@gmail.com>
2021-09-28 17:09:59 +0330

    fix showPageWithSections and apollo client

--- src/pages/[slug]/index.js
+++ src/pages/[slug]/index.js
@@ -4,8 +4,8 @@ import Page from '../../components/Layout/Page'
 import Render from '../../components/Template/Render'
 
 // ** Graphql
-import { client } from '../../graphql/apollo'
-import { SHOW_PAGE, SHOW_SECTIONS } from '../../graphql/queries'
+import { createApolloClient } from '../../graphql/apollo'
+import { SHOW_PAGE_WITH_SECTIONS } from '../../graphql/queries'
 
 export default function Home({ page, sections }) {
   return (
@@ -16,29 +16,20 @@ export default function Home({ page, sections }) {
 }
 
 export async function getServerSideProps({ params }) {
+  const client = createApolloClient()
   const { data: dataPage, error: errorPage } = await client.query({
-    fetchPolicy: 'network-only',
-    query: SHOW_PAGE,
+    query: SHOW_PAGE_WITH_SECTIONS,
     variables: {
       slug: params.slug
     }
   })
 
-  if (dataPage?.showPage && !errorPage) {
-    const { data: dataSections, error: errorSections } = await client.query({
-      fetchPolicy: 'network-only',
-      query: SHOW_SECTIONS,
-      variables: {
-        page: dataPage.showPage.id
-      }
-    })
-
-    if (dataSections?.showSection && !errorSections) {
-      return {
-        props: {
-          page: dataPage.showPage,
-          sections: dataSections.showSection
-        }
+  if (dataPage?.showPageWithSections?.page && dataPage?.showPageWithSections?.sections && !errorPage) {
+    return {
+      props: {
+        page: dataPage.showPageWithSections.page,
+        sections: dataPage.showPageWithSections.sections,
+        apolloState: client.cache.extract()
       }
     }
   }
