e038649e7ccf52dad274824128146b973bc5ddb0
Vigen <vigenpooya@gmail.com>
2021-09-28 17:05:27 +0330

    fix UpdateInsertManySections mutatuin to new section model

--- src/graphql/mutations.js
+++ src/graphql/mutations.js
@@ -110,31 +110,30 @@ export const UPDATE_INSERT_MANY_SECTIONS = gql`
       type
       position
       title
-      links {
-        url
-        title
+      items {
+        id
+        type
+        key
+        value
+        options {
+          key
+          value
       }
-      text
-      contacts {
-        mobile
-        phone
-        email
-        fax
       }
-      messengers {
-        telegram
-        whatsapp
-        twitter
-        youtube
-        linkedin
-      }
-      locations {
-        url
-        title
-      }
-      faq {
-        question
-        answer
+      arrangement
+      customized
+      customize {
+        type
+        rounded
+        animate
+        color
+        second
+        border
+        borderStyle
+        direction
+        from
+        to
+        via
       }
     }
   }
