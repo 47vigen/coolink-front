723adf5e5905985aa0a35ebc3a9d45ffd2703476
Vigen <vigenpooya@gmail.com>
2021-09-28 16:46:21 +0330

    add BRANDS

---
+++ src/config/brands.js
@@ -0,0 +1,236 @@
+export const BRANDS = {
+  telegram: {
+    from: 'blue-300',
+    to: 'blue-400',
+    via: null,
+    direction: 'r'
+  },
+  discord: {
+    from: 'indigo-400',
+    to: 'purple-400',
+    via: null,
+    direction: 'bl'
+  },
+  skype: {
+    from: 'blue-300',
+    to: 'indigo-200',
+    via: null,
+    direction: 'l'
+  },
+  facebook: {
+    from: 'blue-500',
+    to: 'blue-400',
+    via: null,
+    direction: 'l'
+  },
+  whatsapp: {
+    from: 'green-300',
+    to: 'green-400',
+    via: null,
+    direction: 'l'
+  },
+  kik: {
+    from: 'yellow-400',
+    to: 'green-300',
+    via: null,
+    direction: 'tr'
+  },
+  line: {
+    from: 'yellow-300',
+    to: 'green-400',
+    via: null,
+    direction: 'bl'
+  },
+  viber: {
+    from: 'blue-700',
+    to: 'blue-600',
+    via: null,
+    direction: 'l'
+  },
+  instagram: {
+    from: 'yellow-500',
+    to: 'blue-500',
+    via: null,
+    direction: 'l'
+  },
+  youtube: {
+    from: 'red-600',
+    to: 'red-500',
+    via: null,
+    direction: 'tl'
+  },
+  aparat: {
+    from: 'red-400',
+    to: 'red-400',
+    via: null,
+    direction: 'l'
+  },
+  twitter: {
+    from: 'blue-400',
+    to: 'blue-300',
+    via: null,
+    direction: 'tr'
+  },
+  linkedin: {
+    from: 'blue-500',
+    to: 'blue-400',
+    via: null,
+    direction: 'r'
+  },
+  clubhouse: {
+    from: 'gray-900',
+    to: 'gray-700',
+    via: null,
+    direction: 'l'
+  },
+  twitch: {
+    from: 'blue-500',
+    to: 'blue-600',
+    via: null,
+    direction: 'tr'
+  },
+  patreon: {
+    from: 'red-500',
+    to: 'red-400',
+    via: null,
+    direction: 'l'
+  },
+  pinterest: {
+    from: 'red-700',
+    to: 'red-600',
+    via: null,
+    direction: 'br'
+  },
+  tiktok: {
+    from: 'gray-900',
+    to: 'gray-900',
+    via: null,
+    direction: 'l'
+  },
+  anchor: {
+    from: 'blue-600',
+    to: 'blue-700',
+    via: null,
+    direction: 'l'
+  },
+  breaker: {
+    from: 'blue-600',
+    to: 'blue-800',
+    via: null,
+    direction: 'l'
+  },
+  applemusic: {
+    from: 'red-500',
+    to: 'red-400',
+    via: null,
+    direction: 'tl'
+  },
+  castbox: {
+    from: 'yellow-500',
+    to: 'yellow-600',
+    via: null,
+    direction: 'br'
+  },
+  googlepodcasts: {
+    from: 'gray-800',
+    to: 'gray-700',
+    via: null,
+    direction: 'l'
+  },
+  itunes: {
+    from: 'red-400',
+    to: 'blue-500',
+    via: null,
+    direction: 'l'
+  },
+  overcast: {
+    from: 'yellow-600',
+    to: 'yellow-600',
+    via: null,
+    direction: 'l'
+  },
+  pocketcasts: {
+    from: 'red-600',
+    to: 'yellow-600',
+    via: null,
+    direction: 'tl'
+  },
+  podbean: {
+    from: 'green-500',
+    to: 'yellow-400',
+    via: null,
+    direction: 'br'
+  },
+  radiopublic: {
+    from: 'red-700',
+    to: 'red-600',
+    via: null,
+    direction: 'l'
+  },
+  soundcloud: {
+    from: 'yellow-600',
+    to: 'yellow-600',
+    via: null,
+    direction: 'l'
+  },
+  spotify: {
+    from: 'green-500',
+    to: 'green-400',
+    via: null,
+    direction: 'l'
+  },
+  stitcher: {
+    from: 'gray-900',
+    to: 'gray-900',
+    via: null,
+    direction: 'l'
+  },
+  cafebazaar: {
+    from: 'green-400',
+    to: 'yellow-400',
+    via: null,
+    direction: 'tl'
+  },
+  googleplay: {
+    from: 'yellow-200',
+    to: 'purple-400',
+    via: 'red-400',
+    direction: 'l'
+  },
+  appstore: {
+    from: 'blue-400',
+    to: 'indigo-300',
+    via: null,
+    direction: 't'
+  },
+  myket: {
+    from: 'blue-400',
+    to: 'blue-400',
+    via: null,
+    direction: 'l'
+  },
+  lapps: {
+    from: 'blue-600',
+    to: 'blue-400',
+    via: null,
+    direction: 'l'
+  },
+  sibapp: {
+    from: 'blue-500',
+    to: 'blue-500',
+    via: null,
+    direction: 'l'
+  },
+  behance: {
+    from: 'blue-600',
+    to: 'blue-700',
+    via: null,
+    direction: 'l'
+  },
+  dribbble: {
+    from: 'red-400',
+    to: 'red-300',
+    via: null,
+    direction: 'tl'
+  }
+}
