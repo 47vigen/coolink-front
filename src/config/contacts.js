417dc3c47830a13ca0d9c18649082b60dd955e9b
Vigen <vigenpooya@gmail.com>
2021-09-29 18:43:30 +0330

    add contacts config

---
+++ src/config/contacts.js
@@ -0,0 +1,22 @@
+export const contacts = [
+  {
+    label: 'شماره همراه',
+    value: 'mobile',
+    options: [{ key: 'icon', value: 'smartphone' }]
+  },
+  {
+    label: 'شماره تلفن ثابت',
+    value: 'phone',
+    options: [{ key: 'icon', value: 'building' }]
+  },
+  {
+    label: 'ایمیل',
+    value: 'email',
+    options: [{ key: 'icon', value: 'envelope' }]
+  },
+  {
+    label: 'فکس',
+    value: 'fax',
+    options: [{ key: 'icon', value: 'print' }]
+  }
+]
