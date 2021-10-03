8e37a21c9eb1c20bb3c7f0803510716033838616
Vigen <vigenpooya@gmail.com>
2021-10-03 20:59:24 +0330

    add default seo

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,18 @@
 import withApollo from '../graphql/apollo'
 import { AuthProvider } from '../context/auth'
 
+// ** Styles
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
+
+// ** Seo
+import { DefaultSeo } from 'next-seo'
+import SEO from '../config/seo'
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <DefaultSeo {...SEO} />
       <Component {...pageProps} />
     </AuthProvider>
   )
