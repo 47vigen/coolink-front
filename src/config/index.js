c964fa6cdff4e5b16c518dc74ded23b2257269b0
Vigen <vigenpooya@gmail.com>
2021-10-04 18:31:01 +0330

    add customize config

--- src/config/index.js
+++ src/config/index.js
@@ -1,5 +1,6 @@
 import { brands } from './brands'
 import { services } from './services'
 import { contacts } from './contacts'
+import { animate, borderStyle, direction, rounded, type, types } from './customize'
 
-export { brands, services, contacts }
+export { brands, services, contacts, animate, borderStyle, direction, rounded, type, types }
