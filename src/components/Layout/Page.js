0bf3d1a995df7b98f3e864aef9575449fdbb283b
Vigen <vigenpooya@gmail.com>
2021-10-04 19:07:50 +0330

    set progress and chrome tab color to page color

--- src/components/Layout/Page.js
+++ src/components/Layout/Page.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Head from 'next/head'
 import { NextSeo } from 'next-seo'
 
 // ** UI
@@ -14,8 +15,18 @@ import CoolinkLogo from '../../../public/images/coolink-logo.svg'
 
 // ** Utils
 import classNames from '../../utils/classNames'
+import { rgbToHex } from '../../utils/rgbToHex'
+import Seo from '../../config/seo'
 
 function Page({ page, title, children }) {
+  const [themeColor, setThemeColor] = React.useState({ rgb: undefined, hex: undefined })
+
+  React.useEffect(() => {
+    const colorComponent = document.getElementById('color')
+    const color = window.getComputedStyle(colorComponent).getPropertyValue('background-color')
+    setThemeColor({ rgb: color, hex: rgbToHex(color) })
+  }, [])
+
   return (
     <div
       className={classNames(
@@ -26,8 +37,31 @@ function Page({ page, title, children }) {
         backgroundImage: page.style?.background?.url ? `url('${getImgSrc(page.style.background.url)}')` : null
       }}
     >
-      <NextSeo title={title || page.title} titleTemplate={title ? `%s | ${page.title}` : page.subTitle ? `%s | ${page.subTitle}` : '%s'} />
-      <PageHeader page={page} />
+      <Head>
+        <style>
+          {themeColor.rgb
+            ? `
+          #nprogress .bar {
+            background: ${themeColor.rgb} !important;
+          }
+          #nprogress .peg {
+            box-shadow: 0 0 10px ${themeColor.rgb}, 0 0 5px ${themeColor.rgb} !important;
+          }
+          #nprogress .spinner-icon {
+            border-top-color: ${themeColor.rgb} !important;
+            border-left-color: ${themeColor.rgb} !important;
+          }
+        `
+            : null}
+        </style>
+      </Head>
+      <NextSeo
+        {...Seo(themeColor.hex)}
+        title={title || page.title}
+        titleTemplate={title ? `%s | ${page.title}` : page.subTitle ? `%s | ${page.subTitle}` : '%s'}
+      />
+      <PageHeader linked page={page} />
+      <span id="color" className={`sr-only bg-${page?.style?.customize?.color}`}></span>
       <main className="flex-1 container max-w-md mx-auto">{children}</main>
       <footer>
         <Link
