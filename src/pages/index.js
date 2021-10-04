30c385a85df97b36cd31d1e95fab9345427f5a9b
Vigen <vigenpooya@gmail.com>
2021-10-04 20:24:58 +0330

    comment section of comments and add contents

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { useKeenSlider } from 'keen-slider/react'
-import 'keen-slider/keen-slider.min.css'
+// import { useKeenSlider } from 'keen-slider/react'
+// import 'keen-slider/keen-slider.min.css'
 
 // ** UI
 import Layout from '../components/Layout'
@@ -10,44 +10,45 @@ import { Button, Icon } from '../components/Tools'
 import classNames from '../utils/classNames'
 
 export default function Home() {
-  const [sliderRef, slider] = useKeenSlider({
-    slidesPerView: 1,
-    spacing: 16,
-    loop: true,
-    rtl: true,
-    breakpoints: {
-      '(min-width: 640px)': {
-        slidesPerView: 2
-      },
-      '(min-width: 768px)': {
-        slidesPerView: 3
-      },
-      '(min-width: 1024px)': {
-        slidesPerView: 4
-      },
-      '(min-width: 1280px)': {
-        slidesPerView: 5
-      }
-    }
-  })
+  // const [sliderRef, slider] = useKeenSlider({
+  //   slidesPerView: 1,
+  //   spacing: 16,
+  //   loop: true,
+  //   rtl: true,
+  //   breakpoints: {
+  //     '(min-width: 640px)': {
+  //       slidesPerView: 2
+  //     },
+  //     '(min-width: 768px)': {
+  //       slidesPerView: 3
+  //     },
+  //     '(min-width: 1024px)': {
+  //       slidesPerView: 4
+  //     },
+  //     '(min-width: 1280px)': {
+  //       slidesPerView: 5
+  //     }
+  //   }
+  // })
 
   return (
     <Layout>
       <section className="flex flex-col text-center">
         <div className="flex flex-col items-center space-y-2 mt-4">
           <h1 className="text-primary font-bold text-2xl">لینکاتو باحال کن !</h1>
-          <Button bordered className="w-max py-1 px-4">
+          <Button link="/dashboard/create" bordered className="w-max py-1 px-4">
             ایجاد رایگان اکانت کولینک
           </Button>
         </div>
       </section>
       <section className="flex items-center">
         <div className="flex-1 ">
-          <Option icon="alarm-clock" title="لورم ایپسوم">
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و
+          <Option icon="link" title="بی نهایت لینک در بیو اینستاگرامتان داشته باشید">
+            باتوجه به محدودیت اینستاگرام در قرار دادن لینک در بیو با استفاده از کولینک شما می توانید بی نهایت لینک در بیو اینستاگرام خود قرار دهید
           </Option>
-          <Option icon="alarm-clock" title="لورم ایپسوم">
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و
+          <Option icon="edit-alt" title="طراحی اختصاصی">
+            شما می توانید صفحه اختصاصی خود را به طور دلخواه سفارشی سازی کنید و از امکانات بی نظیر کولینک لذت ببرید. همچنین کاربران را با صفحه شیک و
+            خاص خود مجذوب کنید.
           </Option>
         </div>
         <div className="w-2/4 my-7">
@@ -65,15 +66,17 @@ export default function Home() {
           </div>
         </div>
         <div className="flex-1 ">
-          <Option icon="alarm-clock" title="لورم ایپسوم" left>
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و
+          <Option icon="download" title="پست های خود را برای دانلود به اشتراک بگذارید" left>
+            کاربران به طور مستقیم نمی توانند پست های اینستاگرام را دانلود کنند. با کولینک شما می توانید کلیه پست های خود را برای دانلود در دسترس
+            مخاطبانتان قرار دهید.
           </Option>
-          <Option icon="alarm-clock" title="لورم ایپسوم" left>
-            لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و
+          <Option icon="comment" title="شبکه های اجتماعی" left>
+            با استفاده از کولینک می توانید کلیه شبکه های اجتماعی و راه های ارتباطی خود را با کاربران به اشتراک بگذارید و به راحتی کاربران را بین تمام
+            کانال های ارتباطی خود به گردش در بیاورید
           </Option>
         </div>
       </section>
-      <section>
+      {/* <section>
         <h2 className="font-bold">نظرات کاربران کولینک</h2>
         <div className="border-t border-line my-3 pb-3">
           <div ref={sliderRef} className="keen-slider" dir="ltr">
@@ -93,7 +96,7 @@ export default function Home() {
             </Button>
           </div>
         </div>
-      </section>
+      </section> */}
     </Layout>
   )
 }
@@ -110,22 +113,22 @@ const Option = ({ children, icon, title, left }) => {
   )
 }
 
-const Comment = () => {
-  return (
-    <div className="keen-slider__slide">
-      <article className=" flex flex-col my-6" dir="rtl">
-        <div className="flex flex-row items-center">
-          <img src="./images/vigen.jpg" className="w-12 h-auto rounded-lg me-2" />
-          <div className="flex flex-col flex-1">
-            <span>مهدی فراهانی</span>
-            <span className="text-line">@mahtifarahani</span>
-          </div>
-        </div>
-        <p className="border border-line rounded-lg p-2 mt-2">
-          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
-          سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در
-        </p>
-      </article>
-    </div>
-  )
-}
+// const Comment = () => {
+//   return (
+//     <div className="keen-slider__slide">
+//       <article className=" flex flex-col my-6" dir="rtl">
+//         <div className="flex flex-row items-center">
+//           <img src="./images/vigen.jpg" className="w-12 h-auto rounded-lg me-2" />
+//           <div className="flex flex-col flex-1">
+//             <span>مهدی فراهانی</span>
+//             <span className="text-line">@mahtifarahani</span>
+//           </div>
+//         </div>
+//         <p className="border border-line rounded-lg p-2 mt-2">
+//           لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
+//           سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در
+//         </p>
+//       </article>
+//     </div>
+//   )
+// }
