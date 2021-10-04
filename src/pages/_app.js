490630263edfdedef5def34dd8c737f3d36722ee
Vigen <vigenpooya@gmail.com>
2021-10-04 18:09:05 +0330

    add NProgress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -6,10 +6,18 @@ import { AuthProvider } from '../context/auth'
 // ** Styles
 import 'tailwindcss/tailwind.css'
 import '../styles/globals.css'
+import '../styles/progress.css'
 
 // ** Seo
 import { DefaultSeo } from 'next-seo'
 import SEO from '../config/seo'
+
+// ** NProgress
+import Router from 'next/router'
+import NProgress from 'nprogress'
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }) {
   return (
