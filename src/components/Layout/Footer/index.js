0e4eb233685e9143cf3c0afaad3a5d83f61893de
Vigen <vigenpooya@gmail.com>
2021-10-04 19:49:08 +0330

    add social links and fix logo

--- src/components/Layout/Footer/index.js
+++ src/components/Layout/Footer/index.js
@@ -1,20 +1,22 @@
 import React from 'react'
-import Button from '../../Tools/Button'
-import Icon from '../../Tools/Icon'
+
+// ** UI
+import { Button } from '../../Tools'
+
+// ** Images
+import Image from 'next/image'
+import FooterLogo from '../../../../public/images/footer-logo.svg'
 
 function Footer(props) {
   return (
     <footer className="container max-w-screen-xl mx-auto pb-2 px-4">
-      <div className=" flex flex-col items-center space-y-3 border-t border-line pt-4">
-        <img className="w-40 " src="/images/footer-logo.svg" alt="footer-logo" />
-        <p className="text-center">
-          لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و
-          سطرآنچنان که لازم است و برای شرایط
-        </p>
+      <div className="flex flex-col items-center space-y-3 border-t border-line pt-4">
+        <Image src={FooterLogo} alt="Coolink" />
+        <p className="text-center">تمامی حقوق برای کولینک محفوظ است</p>
         <div className="space-s-2">
-          <Button circle>
-            <Icon name="brand-instagram" />
-          </Button>
+          <Button link="mailto://info@colk.ir" icon="envelope" circle />
+          <Button link="https://twitter.com/coolink_ir" icon="brand-twitter" circle />
+          <Button link="https://instagram.com/coolink.ir" icon="brand-instagram" circle />
         </div>
       </div>
     </footer>
