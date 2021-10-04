aa7c78f9d9c572d78ab181115d85dbb4beb20f14
Vigen <vigenpooya@gmail.com>
2021-10-04 18:30:30 +0330

    add customize config

---
+++ src/config/customize.js
@@ -0,0 +1,20 @@
+export const types = ['links', 'text', 'contacts', 'services', 'locations', 'faq', 'igFeedsLink', 'igFeedsDownload']
+export const type = ['default', 'custom', 'gradient']
+export const rounded = ['sm', 'md', 'lg', 'xl', 'full']
+export const animate = [
+  'ping',
+  'pulse',
+  'bounce',
+  'flash',
+  'rubberBand',
+  'shakeX',
+  'shakeY',
+  'headShake',
+  'swing',
+  'tada',
+  'wobble',
+  'jello',
+  'heartBeat'
+]
+export const borderStyle = ['solid', 'dashed', 'dotted', 'double']
+export const direction = ['t', 'l', 'b', 'r', 'tl', 'bl', 'tr', 'br']
