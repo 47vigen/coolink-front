83a6d1166e05175497a21f247a3aaffe2565f5fa
Vigen <vigenpooya@gmail.com>
2021-11-04 20:43:55 +0330

    fix render label

--- src/components/Tools/Listbox.js
+++ src/components/Tools/Listbox.js
@@ -14,9 +14,9 @@ function Listbox({ label, options, value, onChange, renderLabel, renderSelected,
     <HeadlessListbox value={value} onChange={onChange}>
       <div className="relative">
         {label ? <HeadlessListbox.Label className="block mb-2">{label}</HeadlessListbox.Label> : null}
-        <HeadlessListbox.Button className="flex w-full justify-between items-center border border-line transition duration-300 hover:opacity-70 rounded-lg py-2 px-4">
-          {renderSelected ? renderSelected({ option: selected || {} }) : <span className="block truncate">{selected.label}</span>}
-          <Icon name="angle-small-down" />
+        <HeadlessListbox.Button className="flex w-full justify-between items-center border border-line transition duration-300 hover:opacity-70 rounded-lg p-2">
+          {renderSelected ? renderSelected({ option: selected || {} }) : <span className="block truncate ps-2">{selected.label}</span>}
+          <Icon name="angle-small-down" className="pe-2" />
         </HeadlessListbox.Button>
         <Transition as={React.Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
           <HeadlessListbox.Options className="absolute w-full py-1 mt-2 overflow-auto bg-white border border-line rounded-md shadow-2xl max-h-60 smooth-scrollbar focus:outline-none z-10">
