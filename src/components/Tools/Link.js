c4b3691bd9d1161c69297fdd05b3744560fa3cbf
Vigen <vigenpooya@gmail.com>
2021-11-07 00:44:47 +0330

    add props

--- src/components/Tools/Link.js
+++ src/components/Tools/Link.js
@@ -2,10 +2,10 @@ import React from 'react'
 import NextLink from 'next/link'
 import classNames from '../../utils/classNames'
 
-function Link({ children, href, className, style, replace }) {
+function Link({ children, href, className, style, replace, ...props }) {
   return (
     <NextLink replace={replace} href={href}>
-      <a style={style} className={classNames('relative cursor-pointer hover:text-primary transition ease-in-out duration-200', className)}>
+      <a style={style} className={classNames('relative cursor-pointer hover:text-primary transition ease-in-out duration-200', className)} {...props}>
         {children}
       </a>
     </NextLink>
