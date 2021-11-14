50c63b4033d6f2c061023fdb911ac2b52e882406
Vigen <vigenpooya@gmail.com>
2021-11-14 19:19:36 +0330

    add wrapperName prop

--- src/components/Layout/Header/index.js
+++ src/components/Layout/Header/index.js
@@ -13,49 +13,47 @@ import CoolinkLogo from '../../../../public/images/coolink-logo.svg'
 import classNames from '../../../utils/classNames'
 import onOutside from '../../../utils/onOutside'
 
-function Header(props) {
+function Header({ wrapperName }) {
   const { user } = useAuth()
 
   const [open, setOpen] = React.useState(false)
   onOutside('mobile-navbar', () => setOpen(false))
 
   return (
-    <>
-      <header className="container m-auto mb-4 p-4 flex justify-between items-center">
-        <div className="md:w-40 flex items-center">
-          <Button
-            type="ghost"
-            icon="menu-burger"
-            className="-my-2 me-4 pe-4 -ms-2 block lg:hidden bg-transparent border-line border-e !min-h-0 h-[min-content] py-2 rounded-none"
-            onClick={() => setOpen(true)}
-          />
-          <UserState />
-        </div>
-        <nav
-          id="mobile-navbar"
-          className={classNames(
-            'absolute start-0 end-0 flex bg-white top-0 py-3 px-4 z-10 transform transition-all lg:relative lg:p-0 lg:bg-transparent lg:shadow-none lg:z-0 lg:translate-y-0',
-            open ? 'shadow-lg' : '-translate-y-11'
-          )}
-        >
-          <ul className="flex space-s-5 ">
-            <li>
-              <Link href="/">خانه</Link>
-            </li>
-            <li>
-              <Link href="https://virgool.io/coolink">وبلاگ</Link>
-            </li>
-            <li>
-              <Link href={user.id ? '/dashboard/create' : '/sign-up?ref=/dashboard/create'}>شروع کنید!</Link>
-            </li>
-          </ul>
-          <Button type="ghost" icon="angle-small-up" className="block lg:hidden ms-auto -m-2" onClick={() => setOpen(false)} />
-        </nav>
-        <Link href="/" className="md:w-40 ms-4 lg:ms-0">
-          <Image src={CoolinkLogo} alt="Coolink" />
-        </Link>
-      </header>
-    </>
+    <header className={classNames('container m-auto mb-4 p-4 flex justify-between items-center', wrapperName)}>
+      <div className="md:w-40 flex items-center">
+        <Button
+          type="ghost"
+          icon="menu-burger"
+          className="-my-2 me-4 pe-4 -ms-2 block lg:hidden bg-transparent border-line border-e !min-h-0 h-[min-content] py-2 rounded-none"
+          onClick={() => setOpen(true)}
+        />
+        <UserState />
+      </div>
+      <nav
+        id="mobile-navbar"
+        className={classNames(
+          'absolute start-0 end-0 flex bg-white top-0 py-3 px-4 z-10 transform transition-all lg:relative lg:p-0 lg:bg-transparent lg:shadow-none lg:z-0 lg:translate-y-0',
+          open ? 'shadow-lg' : '-translate-y-11'
+        )}
+      >
+        <ul className="flex space-s-5 ">
+          <li>
+            <Link href="/">خانه</Link>
+          </li>
+          <li>
+            <Link href="https://virgool.io/coolink">وبلاگ</Link>
+          </li>
+          <li>
+            <Link href={user.id ? '/dashboard/create' : '/sign-up?ref=/dashboard/create'}>شروع کنید!</Link>
+          </li>
+        </ul>
+        <Button type="ghost" icon="angle-small-up" className="block lg:hidden ms-auto -m-2" onClick={() => setOpen(false)} />
+      </nav>
+      <Link href="/" className="md:w-40 ms-4 lg:ms-0">
+        <Image src={CoolinkLogo} alt="Coolink" />
+      </Link>
+    </header>
   )
 }
 
