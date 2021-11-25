4d99158ffb6f4161bed168e2558384776d3765d4
Vigen <vigenpooya@gmail.com>
2021-11-25 17:30:07 +0330

    fix browser schema again

--- src/utils/onDeepLink.js
+++ src/utils/onDeepLink.js
@@ -71,7 +71,7 @@ export const generateDeepLink = (type = '', link = '') => {
 
     case 'browser':
       return {
-        'deep-link': 'ftp://coolink.ir/bridge.html?u=' + link,
+        'deep-link': 'ftp://https://coolink.ir/bridge.html?u=' + link,
         'deep-link-ad': 'intent://' + link.replace('https://', '').replace('http://', '') + '#Intent;scheme=http;package=com.android.chrome;end',
         url: link
       }
