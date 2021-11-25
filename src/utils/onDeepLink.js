aa65b8dd09447bbb1c115ed9d82cd417ac22bdb6
Vigen <vigenpooya@gmail.com>
2021-11-25 16:16:40 +0330

    fix browser schema again

--- src/utils/onDeepLink.js
+++ src/utils/onDeepLink.js
@@ -71,8 +71,8 @@ export const generateDeepLink = (type = '', link = '') => {
 
     case 'browser':
       return {
-        'deep-link': 'safari://' + link,
-        'deep-link-ad': 'intent://' + link + '#Intent;scheme=http;package=com.android.chrome;end',
+        'deep-link': 'safari://' + link.replace('https://', '').replace('http://', ''),
+        'deep-link-ad': 'intent://' + link.replace('https://', '').replace('http://', '') + '#Intent;scheme=http;package=com.android.chrome;end',
         url: link
       }
   }
