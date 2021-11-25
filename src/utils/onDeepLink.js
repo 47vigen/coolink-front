fd0ac39bd7d099a1d44b6e26171797f6ac728a61
Vigen <vigenpooya@gmail.com>
2021-11-25 19:34:17 +0330

    add new deeplinker

--- src/utils/onDeepLink.js
+++ src/utils/onDeepLink.js
@@ -113,26 +113,118 @@ const openUrlWithFallback = (t, e) => {
     (a.src = t)
 }
 
+class DeepLinker {
+  constructor(options) {
+    if (!options) {
+      throw new Error('no options')
+    }
+
+    let hasFocus = true
+    let didHide = false
+
+    // window is blurred when dialogs are shown
+    function onBlur() {
+      hasFocus = false
+    }
+
+    // document is hidden when native app is shown or browser is backgrounded
+    function onVisibilityChange(e) {
+      if (e.target.visibilityState === 'hidden') {
+        didHide = true
+      }
+    }
+
+    // window is focused when dialogs are hidden, or browser comes into view
+    function onFocus() {
+      if (didHide) {
+        if (options.onReturn) {
+          options.onReturn()
+        }
+
+        didHide = false // reset
+      } else {
+        // ignore duplicate focus event when returning from native app on
+        // iOS Safari 13.3+
+        if (!hasFocus && options.onFallback) {
+          // wait for app switch transition to fully complete - only then is
+          // 'visibilitychange' fired
+          setTimeout(function () {
+            // if browser was not hidden, the deep link failed
+            if (!didHide) {
+              options.onFallback()
+            }
+          }, 1000)
+        }
+      }
+
+      hasFocus = true
+    }
+
+    // add/remove event listeners
+    // `mode` can be "add" or "remove"
+    function bindEvents(mode) {
+      ;[
+        [window, 'blur', onBlur],
+        [document, 'visibilitychange', onVisibilityChange],
+        [window, 'focus', onFocus]
+      ].forEach(function (conf) {
+        conf[0][mode + 'EventListener'](conf[1], conf[2])
+      })
+    }
+
+    // add event listeners
+    bindEvents('add')
+
+    // expose public API
+    this.destroy = bindEvents.bind(null, 'remove')
+    this.openURL = function (url) {
+      // it can take a while for the dialog to appear
+      let dialogTimeout = 500
+
+      setTimeout(function () {
+        if (hasFocus && options.onIgnored) {
+          options.onIgnored()
+        }
+      }, dialogTimeout)
+
+      window.location = url
+    }
+  }
+}
+
 const onDeepLink = (type, link) => {
   const urls = generateDeepLink(type, link)
+
   if (typeof window !== 'undefined') {
     const os = new UAParser().getOS().name
+    const href = window.location.href
+    const linker = new DeepLinker({
+      onIgnored: function () {
+        console.log('browser failed to respond to the deep link')
+        window.open(urls.url, '_self')
+      },
+      onReturn: function () {
+        if (window.location.href !== href) window.location.replace(href)
+      }
+    })
+
     if (urls['deep-link-ad'] && os === 'Android') {
       return {
         url: urls.url,
         onClick: () => {
-          openUrlWithFallback(urls['deep-link-ad'], urls.url)
+          linker.openURL(urls['deep-link-ad'])
         }
       }
     } else if (urls['deep-link'] && os !== 'Android') {
       return {
         url: urls.url,
         onClick: () => {
-          openUrlWithFallback(urls['deep-link'], urls.url)
+          linker.openURL(urls['deep-link'])
         }
       }
     }
   }
+
   return { url: urls.url }
 }
 
