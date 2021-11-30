81cf84c0e670c79e2dfe5b2286877cd664400f89
Vigen <vigenpooya@gmail.com>
2021-11-30 20:20:00 +0330

    fix video player

--- src/components/Feeds/FeedImage.js
+++ src/components/Feeds/FeedImage.js
@@ -1,20 +1,33 @@
 import React from 'react'
 import Image from 'next/image'
-import { Player } from 'video-react'
-import 'video-react/dist/video-react.css'
 
 // ** Utils
 import { getImgSrc } from '../../utils/getImgSrc'
 import classNames from '../../utils/classNames'
 
 const FeedImage = ({ idx = 0, feed, opened, className }) => {
+  const ref = React.useRef()
+  const [fullScreen, setFullScreen] = React.useState(false)
   const imgSrc = React.useMemo(() => getImgSrc(feed.slides[idx].imageUrl), [idx, feed.slides])
   const videoSrc = React.useMemo(() => (feed.slides[idx].videoUrl ? getImgSrc(feed.slides[idx].videoUrl) : null), [idx, feed.slides])
 
+  React.useEffect(() => {
+    ref.current?.addEventListener('fullscreenchange', (event) => {
+      setFullScreen(!event.target?.offsetLeft)
+    })
+  })
+
   return opened && videoSrc ? (
-    <Player poster={imgSrc}>
-      <source src={videoSrc} />
-    </Player>
+    <video
+      controls
+      ref={ref}
+      width={1080}
+      height={1080}
+      poster={imgSrc}
+      className={classNames(!fullScreen ? 'object-cover h-[calc(100vw-1rem)] max-h-[28rem] rounded-md' : '', className)}
+    >
+      <source src={videoSrc} type="video/mp4" />
+    </video>
   ) : (
     <Image
       src={imgSrc}
