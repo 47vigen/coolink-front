058c4a86c6bf9c84c7042407fad487d4788c8e4c
Vigen <vigenpooya@gmail.com>
2021-11-30 19:12:17 +0330

    fix grid

--- src/components/Feeds/FeedItem.js
+++ src/components/Feeds/FeedItem.js
@@ -9,7 +9,7 @@ function FeedItem({ feed, page, section }) {
 
   return (
     <article>
-      <Link href={`/${page.slug}/feeds/${feed.pk}`} className="block relative">
+      <Link href={`/${page.slug}/feeds/${feed.pk}`} className="flex relative">
         <FeedImage feed={feed} className={`bg-${custom(1).color || 'white'}`} />
         {feed.slides.length > 1 ? <FeedFlag type="slide" /> : feed.slides[0].type === 'video' ? <FeedFlag type="video" /> : null}
       </Link>
